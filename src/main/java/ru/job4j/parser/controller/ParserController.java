cb831bbd05e8e7453e9ec26096f9cf63bf944473
Vladimir Likhachev <lettervl@ya.ru>
2023-01-06 15:19:49 +0300

    2. JS to Java

--- src/main/java/ru/job4j/parser/controller/ParserController.java
+++ src/main/java/ru/job4j/parser/controller/ParserController.java
@@ -1,23 +1,28 @@
 package ru.job4j.parser.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.job4j.parser.utils.ParserUtil;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
 public class ParserController{
 
+    private final ParserUtil parserUtil;
+
+    public ParserController(ParserUtil parserUtil) {
+        this.parserUtil = parserUtil;
+    }
+
     @PostMapping("/parser")
-    public List<String> getList(Model model, @RequestParam(name = "url", required = false) String url) {
-        List<String> str = new ArrayList<>();
-        log.info(url);
-        return str;
+    public List<String> getList(Model model, @RequestParam String url) {
+        log.info("Поступил запрос на парсинг сайта {}", url);
+        return parserUtil.getLinks(url);
     }
 
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,23 +1,25 @@
-$(function() {
+$(document).ready(function() {
    $(':submit').click(function() {
-      let link = ($('#link').val());
-      let URL = link;
+      let URL = ($('#link').val());
       parser(URL);
    })
 
-});
-
-
 function parser(URL) {
-   $.ajax({
-       url: '/parser',
-       type: 'post',
-       data: {
-           url: URL
-       },
-       success: function (data) {
-           alert(data.toString());
-       }
-   });
-}
+    $.ajax({
+        url: '/parser',
+        type: 'post',
+        data: {
+        url: URL
+        },
+        success: function (data) {
+            data.forEach(link => {
+                let attr = `<p style="text-align:left; margin-left:25%;"><a href="#">автомобиль</a></p>`;
+                $('.form1').append(attr);
+                $('#href').attr("href", link);
+                })
+            }
+        });
+    }
+
+});
 
